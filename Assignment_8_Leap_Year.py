b1f9d452a43352eec6c6b23b86e783f92c2ff856
htckbr <haticekubraceylan@gmail.com>
2020-07-22 12:40:57 +0300

    github assignment8 yükleme 2

---
+++ Assignment_8_Leap_Year.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Leap Year
+
+# In[9]:
+
+
+x = int(input('Enter a year : '))
+def leap(x):
+    return (lambda x: print(x,"is a leap year") if x%4==0 and x%100!=0 or x%400==0 else print(x,'is not a leap year'))(x)
+leap(x)
+
+
+# In[ ]:
+
+
+
+
